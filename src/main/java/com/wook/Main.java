c45d0070904252cc54a0ebf3292b293dd006180d
w00k <wook82@gmail.com>
2021-09-21 12:37:04 -0300

    add first commit

--- src/main/java/com/wook/Main.java
+++ src/main/java/com/wook/Main.java
@@ -1,2 +1,35 @@
-package com.wook;public class Main {
+package com.wook;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("anagram false : " + isAnagram("LISTEN", "sfsdfsfd"));
+        System.out.println("anagram true : " + isAnagram("race", "care"));
+    }
+
+    //order alphabetical and return a string
+    public static String orderAlphabetical(String str) {
+        char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+
+    //return true when is a acronimus
+    public static boolean isAnagram(String strOne, String strTwo) {
+        String orderOne = null, orderTwo = null;
+        boolean response = false;
+
+        orderOne = orderAlphabetical(strOne);
+        orderTwo = orderAlphabetical(strTwo);
+
+        if (orderOne.equalsIgnoreCase(orderTwo) == true) {
+            response = true;
+        }
+
+        return response;
+    }
 }
+
